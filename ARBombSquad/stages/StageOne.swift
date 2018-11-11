647820eec9617666347e1bc255bfb09b4917f71a
Akshat Goel <akshatishu@gmail.com>
2018-11-10 23:55:07 -0800

    Added bomb int 3d model and added the bomb 3d model in stage1 and stage2.

--- ARBombSquad/stages/StageOne.swift
+++ ARBombSquad/stages/StageOne.swift
@@ -15,14 +15,17 @@ class StageOne: NSObject {
     
     func configurationForStage() -> (Array<SCNNode>, Int, TimeInterval) {  //eggs, tries, time
         
-        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
-        let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+        let eggScene = SCNScene(named: "Media.scnassets/tnt.scn")
+        let eggNode = (eggScene?.rootNode.childNode(withName: "tnt", recursively: false))!
         eggNode.name = "egg"
         eggNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: eggNode, options: nil))
         eggNode.physicsBody?.categoryBitMask = BitMaskCategory.target.rawValue
         eggNode.physicsBody?.contactTestBitMask = BitMaskCategory.bullet.rawValue
         
         eggNode.position = SCNVector3(x: 0, y: 0, z: -40)
+        let bombAnimation =  SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians),z:0, duration: 5)
+        let foreverAction = SCNAction.repeatForever(bombAnimation)
+        eggNode.runAction(foreverAction)
         eggs?.append(eggNode)
         return (eggs!, 0, 0)
     }
--- ARBombSquad/stages/StageTwo.swift
+++ ARBombSquad/stages/StageTwo.swift
@@ -15,19 +15,24 @@ class StageTwo: NSObject {
     
     func configurationForStage() -> (Array<SCNNode>, Int, TimeInterval) { //eggs, tries, time
         for i in 0...2 {
-            let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
-            let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+            let eggScene = SCNScene(named: "Media.scnassets/tnt.scn")
+            let eggNode = (eggScene?.rootNode.childNode(withName: "tnt", recursively: false))!
             eggNode.name = "egg"
             eggNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: eggNode, options: nil))
             eggNode.physicsBody?.categoryBitMask = BitMaskCategory.target.rawValue
             eggNode.physicsBody?.contactTestBitMask = BitMaskCategory.bullet.rawValue
+            let bombAnimation =  SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians),z:0, duration: 5)
+            let foreverAction = SCNAction.repeatForever(bombAnimation)
             if i == 0 {
                 eggNode.position = SCNVector3(x: 0, y: 20, z: -40)
+                 eggNode.runAction(foreverAction)
             }
             else if i == 1 {
                 eggNode.position = SCNVector3(x: -10, y: -5, z: -40)
+                 eggNode.runAction(foreverAction)
             } else{
                 eggNode.position = SCNVector3(x: 10, y: -5, z: -40)
+                 eggNode.runAction(foreverAction)
             }
             eggs?.append(eggNode)
         }
