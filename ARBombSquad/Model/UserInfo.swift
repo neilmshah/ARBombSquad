3eaa6f1195e82c13238f8c68b6d7021f0b7c63f9
Neil Shah <neilmshah01@gmail.com>
2018-11-30 12:08:41 -0800

    working on UserInfo parse backend

--- ARBombSquad/Model/UserInfo.swift
+++ ARBombSquad/Model/UserInfo.swift
@@ -3,7 +3,22 @@
 //  ARBombSquad
 //
 //  Created by Neil Shah on 11/30/18.
-//  Copyright © 2018 starksky. All rights reserved.
+//  Copyright © 2018 Neil. All rights reserved.
 //
 
 import Foundation
+import Parse
+
+class UserInfo: PFObject, PFSubclassing {
+    
+    @NSManaged var author: PFUser
+    @NSManaged var score: String
+    @NSManaged var level: Int
+    
+    static func parseClassName() -> String {
+        return "UserInfo"
+    }
+    
+    
+
+}
